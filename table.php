19f7736a723b7810aa2b404194f98282874c2116
Ronando Long <ronando.long@gmail.com>
2017-01-18 18:43:49 -0800

    add files to take in user input number and print to browser times table the size of number x number

---
+++ table.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>Times Table</title>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+    
+  </head>
+
+  <body>
+    <div class='container'>
+      <h1>Table Creation</h1>
+
+      <form action='times.php'>
+        <div class='form-group'>
+          <label for='number'>Enter a number to make a times table with</label>
+          <input type='number' name='number' id='number'>
+        </div>
+        <button type='submit' class='btn btn-primary'>Submit</button>
+
+      </form>
+    </div>
+  </body>
+</html>"
---
+++ times.php
@@ -0,0 +1,44 @@
+<?php
+    $number = $_GET["number"];
+    $array = range(1, $number);
+    $counter_array = $array;
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>Times Table</title>
+    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+  </head>
+
+  <body>
+    <div class='container'>
+      <h1>The Times Table</h1>
+      <?php
+          echo "<table>";
+              // Top Row
+          echo "<tr><th>&nbsp;</th>";
+          foreach ($array as $i) {
+              echo "<th> $i </th>";
+          }
+          echo "</tr>";
+
+              // Remaining Rows
+          foreach ($counter_array as $row) {
+            echo "<tr><th> $row </th>";   // !!!!! Don't forget to close this <tr>
+            foreach ($array as $r) {
+              echo "<td>" . ($r * $row) . "</td>";
+            }
+            echo "</tr>";
+          }
+
+          echo "</tr></table>";
+      ?>
+
+    </div>
+  </body>
+</html>
